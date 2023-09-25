839a5459ecedf7eef19e98b7dbaee8a811dd07ff
CSdiez <gonzalez-braian@hotmail.com>
2023-09-25 05:29:01 -0300

    seven commit

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ $(function(){
       config('./assets/gallery', 3, 7000, 1600, 1);
       livebackground();
     })
+
+    $(".fav").click(function() {
+        $(this).find('img').toggle();
+    });
